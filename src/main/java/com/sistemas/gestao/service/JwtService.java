2ad5e62f81989f92ea293ed7d8ebdb835ba8e1c5
AnaGCastro <testeanateste0@gmail.com>
2025-05-23 15:06:21 -0300

    Classe JwtService implementada e pom atualizado

--- src/main/java/com/sistemas/gestao/service/JwtService.java
+++ src/main/java/com/sistemas/gestao/service/JwtService.java
@@ -1,10 +1,34 @@
 package com.sistemas.gestao.service;
 
+import org.springframework.stereotype.Service;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Service
 public class JwtService {
 
+	private final SecretKey SECRET_KEY = Keys.hmacShaKeyFor("14789614789614789614789614789614".getBytes(StandardCharsets.UTF_8));
+
 	public String generateToken(String username) {
-		// TODO Auto-generated method stub
-		return null;
+		return Jwts.builder()
+				.setSubject(username)
+				.setIssuedAt(new Date())
+				.setExpiration(new Date(System.currentTimeMillis() + 1 * 60 * 60 * 1000)) // 1 hora
+				.signWith(SECRET_KEY, SignatureAlgorithm.HS256)
+				.compact();		
 	}
 
+	public String extractUsername(String token) {
+		return Jwts.parserBuilder()
+				.setSigningKey(SECRET_KEY)
+				.build()
+				.parseClaimsJws(token)
+				.getBody()
+				.getSubject();
+	}
 }
