f99327debb7842db60a3a7d42ece89c74d05c937
AnaGCastro <testeanateste0@gmail.com>
2025-05-23 17:59:14 -0300

    Login iniciado

--- src/main/frontend/bares-frontend/src/app/components/login/login.component.ts
+++ src/main/frontend/bares-frontend/src/app/components/login/login.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  standalone: true,
-  imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  username = '';
+  password = '';
+  error = '';
 
+  constructor(private authService: AuthService, private router: Router) {}
+
+  onSubmit(): void {
+    this.authService.login({ username: this.username, password: this.password }).subscribe({
+      next: (res) => {
+        localStorage.setItem('token', res.token);
+        this.router.navigate(['/']);
+      },
+      error: () => {
+        this.error = 'Usuário ou senha inválidos.';
+      }
+    });
+  }
 }
+
