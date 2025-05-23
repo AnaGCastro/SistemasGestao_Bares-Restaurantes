44dd699d0b910e42b772e0feed13f64a9220fa9c
AnaGCastro <testeanateste0@gmail.com>
2025-05-23 13:43:03 -0300

    Classe User Service foi implementada

---
+++ src/main/java/com/sistemas/gestao/UserService.java
@@ -0,0 +1,21 @@
+package com.sistemas.gestao;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sistemas.gestao.entities.User;
+import com.sistemas.gestao.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	public Optional<User> authenticate(String username, String password) {
+		return userRepository.findByUsernameAndPassword(username, password);
+	}
+
+}
