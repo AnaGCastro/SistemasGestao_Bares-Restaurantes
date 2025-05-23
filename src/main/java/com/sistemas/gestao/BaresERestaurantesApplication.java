9f4c60dffbd930afe07eecdbcca70f537b38bbf0
AnaGCastro <testeanateste0@gmail.com>
2025-05-23 12:02:46 -0300

    Classe User implementada

---
+++ src/main/java/com/sistemas/gestao/BaresERestaurantesApplication.java
@@ -0,0 +1,13 @@
+package com.sistemas.gestao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaresERestaurantesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaresERestaurantesApplication.class, args);
+	}
+
+}
